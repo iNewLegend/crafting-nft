5a4233e13c0dc0c064f6262d98d513a60ad136da
Leonid Vinikov <czf.leo123@gmail.com>
2024-05-29 07:17:10 +0300

    feat: add metmask detect provider - Adds metamask detect provider to package.json - Updates create NFT tab to detect ethereum provider - Updates package-lock.json

--- src/components/tabs/tab-create-nft.tsx
+++ src/components/tabs/tab-create-nft.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 
+import detectEthereumProvider from "@metamask/detect-provider";
+
 import { Button, Image, Input, Textarea } from "@nextui-org/react";
 
-export function TabCreateNFT() {
-    const [ name, setName ] = React.useState( "" );
-    const [ description, setDescription ] = React.useState( "" );
-    const [ image, setImage ] = React.useState<string | null>( null );
+import use from "../../utils/react-use.ts";
+
+function CreateNFTForm( props: {
+    setImage: ( value: ( ( ( prevState: ( string | null ) ) => ( string | null ) ) | string | null ) ) => void,
+    name: string,
+    setName: ( value: ( ( ( prevState: string ) => string ) | string ) ) => void,
+    description: string,
+    setDescription: ( value: ( ( ( prevState: string ) => string ) | string ) ) => void,
+    image: string | null,
+    provider: ReturnType<Awaited<typeof detectEthereumProvider>> | null
+} ) {
+    const { setImage, name, setName, description, setDescription, image, provider } = props;
+
+    if ( ! provider ) {
+        return <h1>To create an NFT, you need to have MetaMask installed.</h1>
+    }
 
     const handleImageUpload = ( event: React.ChangeEvent<HTMLInputElement> ) => {
         const file = event.target.files?.[ 0 ];
@@ -44,5 +58,26 @@ export function TabCreateNFT() {
                 Create NFT
             </Button>
         </div>
+    );
+}
+
+export function TabCreateNFT() {
+    const [name, setName] = React.useState("");
+    const [description, setDescription] = React.useState("");
+    const [image, setImage] = React.useState<string | null>(null);
+
+    // This will trigger the `Suspense` above...
+    const provider = use( detectEthereumProvider );
+
+    return (
+        <CreateNFTForm
+            setImage={setImage}
+            name={name}
+            setName={setName}
+            description={description}
+            setDescription={setDescription}
+            image={image}
+            provider={provider}
+        />
     );
 }
