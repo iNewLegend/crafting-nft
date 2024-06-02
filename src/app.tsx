34976f894e523999808b3453d9c11ca7900aadac
Leonid Vinikov <czf.leo123@gmail.com>
2024-06-02 20:03:16 +0300

    feat(layout): overhaul interface and improve structure - Delete old `layout.scss` and replace with new optimized `layout.module.scss` - Introduce navbar design and features - Create new `app.scss` to handle app-wide styles - Add themed variables in newly created `vars.scss` - Update `app.tsx` to include new Pages component and conditional rendering based on wallet status - Update `layout.tsx` to incorporate header, divider, and card body, using new styling from `layout.module.scss`

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 
 import Layout from "./components/layout/layout";
+import Pages from "./components/pages/pages.tsx";
 import LoadingSpinner from "./components/loading/loading-spinner";
 import WalletConnect from "./components/wallet/wallet-connect";
 
+import type { IWallet } from "./modules/wallet/wallet-definitions.ts";
+
+import "./app.scss";
+
 function App() {
+    const [ wallet, setWallet ] = React.useState<IWallet | null>( null );
+
+    const Header: React.FC<{ wallet: IWallet | null }> = ( { wallet } ) => {
+        if ( wallet ) {
+            return <h1>Welcome</h1>;
+        }
+
+        return <>Select Wallet</>;
+    };
+
     return (
-        <Layout>
+        <Layout header={ <Header wallet={ wallet }/> }>
             <React.Suspense fallback={ <LoadingSpinner/> }>
-                <WalletConnect/>
+                <WalletConnect onSync={ setWallet }>
+                    <Pages/>
+                </WalletConnect>
             </React.Suspense>
         </Layout>
     );
