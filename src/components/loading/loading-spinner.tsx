e747294d06250a8ea3152756c3db0daa6ecc104b
Leonid Vinikov <czf.leo123@gmail.com>
2024-06-03 15:02:34 +0300

    tweak(loading-spinner): Align spinner to center using tailwind css

--- src/components/loading/loading-spinner.tsx
+++ src/components/loading/loading-spinner.tsx
@@ -1,5 +1,5 @@
 import { Spinner } from "@nextui-org/spinner";
 
 export default function LoadingSpinner() {
-    return <Spinner className="p-5" label="Loading..." color="primary" />
+    return <Spinner className="p-5 flex justify-center" label="Loading..." color="primary" />
 }
